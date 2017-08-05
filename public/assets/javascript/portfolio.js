0564d3e27482285b96ecf1880902d04e407a2cb6
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-08-05 18:55:52 -0500

    added print function

--- public/assets/javascript/portfolio.js
+++ public/assets/javascript/portfolio.js
@@ -1,21 +1,7 @@
 $(document).ready(function() {
 
-	$("#pdf").on('click', function() {
-
-		var doc = new jsPDF();
-
-		var specialElementHandlers = {
-			'#editor': function(element, renderer){
-				return true;
-			}
-		};
-
-		doc.fromHTML($('.container').get(0), 15, 15, {
-			'width': 170, 
-			'elementHandlers': specialElementHandlers
-		});
-		doc.save("myfoldableportfolio");
-		doc.output('dataurlnewwindow'); 
+	$("#printBtn").on('click', function() {
+		window.print();
 	});
 
 	// oriDomi element for the landing page
