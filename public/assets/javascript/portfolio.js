12329776c5e87992132417cdc78b79bb1d9d520f
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-08-07 23:16:49 -0500

    added styling

--- public/assets/javascript/portfolio.js
+++ public/assets/javascript/portfolio.js
@@ -4,23 +4,5 @@ $(document).ready(function() {
 		window.print();
 	});
 
-	// oriDomi element for the landing page
-	var oriDomi = new OriDomi(".foldedText", {
-	    vPanels: 5,
-	    speed: 1500,
-	    ripple: true,
-	    touchEndCallback: function() {
-	        console.log('touch ended');
-		}
-	});
-	oriDomi.accordion(40);
-
-	$(".foldedText").on("mouseover", function() {
-		var num = Math.floor(Math.random() * 40 + 30);
-		console.log(num);
-    	oriDomi.accordion(num);
-    });
-
-
 }); // document.ready
 
