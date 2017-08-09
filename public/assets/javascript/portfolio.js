af910d5324bd0c47233a4db19baec775adeda647
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-08-09 18:59:28 -0500

    minor style changes

--- public/assets/javascript/portfolio.js
+++ public/assets/javascript/portfolio.js
@@ -10,20 +10,6 @@ $(document).ready(function() {
 		window.print();
 	});
 
-	// function to convert user name to all caps, and to get the color code
-	// var fName;
-	// var lName;
-	// var backgroundColor;
-	// $("#add").on("click", function() {
-	// 	fName = $("#firstName").val().trim().toUpperCase();
-	// 	lName = $("#lastName").val().trim().toUpperCase();
-	// 	backgroundColor = "#" + ($("#bgColor").val().trim());
-	// 	console.log(fName);
-	// 	console.log(lName);
-	// 	console.log(backgroundColor);
-	// 	return true;
-	// 	changeColor();
-	// });
 
 }); // document.ready
 
